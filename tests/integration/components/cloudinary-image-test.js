0932931dae99281483ae4e9ffaf3424b7d531b05
Miles Zimmerman <miles@maztek.io>
2016-02-25 13:53:39 -0800

    Adding one more test

--- tests/integration/components/cloudinary-image-test.js
+++ tests/integration/components/cloudinary-image-test.js
@@ -24,3 +24,12 @@ test('it renders an image with width and height options', function(assert) {
   assert.equal(this.$('img').attr('width'), 100);
   assert.equal(this.$('img').attr('height'), 100);
 });
+
+
+test('it renders an image with width and height options in url and attributes', function(assert) {
+  this.render(hbs`{{cloudinary-image 'test' (hash width=100 height=100 crop='fill')}}`);
+
+  assert.equal(this.$('img').attr('src'), 'https://res.cloudinary.com/cloud-name/image/upload/c_fill,h_100,w_100/test');
+  assert.equal(this.$('img').attr('width'), 100);
+  assert.equal(this.$('img').attr('height'), 100);
+});
